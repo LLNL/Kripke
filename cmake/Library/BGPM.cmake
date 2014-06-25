12319fc558b231f4bb3384f1ada9d5c9a434ed11
Adam J. Kunen <kunen1@llnl.gov>
2014-06-25 11:24:04 -0700

    Added Bob Walkup's BG/Q trace library

---
+++ cmake/Library/BGPM.cmake
@@ -0,0 +1,11 @@
+
+set(BGPM_DEFAULT_INCLUDE_DIRS "/usr/local/tools/mpitrace/include")
+
+set(BGPM_DEFAULT_LIB_DIR /bgsys/drivers/ppcfloor/bgpm/lib /usr/local/tools/mpitrace/lib)
+set(BGPM_DEFAULT_LIBS libbgpm.a libmpihpm_r.a)
+
+set(BGPM_DEFAULT_DEFINITIONS "-DKRIPKE_USE_BGPM")
+
+
+
+
