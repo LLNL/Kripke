0cbac4bc523be23f649341908572345d32213ab0
Adam J. Kunen <kunen1@llnl.gov>
2014-07-02 09:20:42 -0700

    Fixed PAPI library includes (papi not PAPI). Switched to better XLC compiler on BG/Q

---
+++ cmake/Library/papi.cmake
@@ -0,0 +1,11 @@
+
+set(papi_DEFAULT_INCLUDE_DIRS "${papi_PREFIX}/static/include")
+
+set(papi_DEFAULT_LIB_DIR "${papi_PREFIX}/static/lib")
+set(papi_DEFAULT_LIBS "papi")
+
+set(papi_DEFAULT_DEFINITIONS "-DKRIPKE_USE_PAPI")
+
+
+
+
--- cmake/Toolchain/bgqos_0-xlc.cmake
+++ cmake/Toolchain/bgqos_0-xlc.cmake
@@ -1,12 +1,19 @@
 
-set(CMAKE_C_COMPILER mpixlcxx_r)
+#set(CMAKE_C_COMPILER mpixlcxx_r)
 
-set(CMAKE_CXX_COMPILER mpixlcxx_r)
+#set(CMAKE_CXX_COMPILER mpixlcxx_r)
 
-set(CMAKE_LINKER mpixlcxx_r)
+#set(CMAKE_LINKER mpixlcxx_r)
+
+set(CMAKE_C_COMPILER /usr/local/tools/compilers/ibm/mpixlc_r-lompbeta2-fastmpi)
+
+set(CMAKE_CXX_COMPILER /usr/local/tools/compilers/ibm/mpixlcxx_r-lompbeta2-fastmpi)
+
+set(CMAKE_LINKER /usr/local/tools/compilers/ibm/mpixlcxx_r-lompbeta2-fastmpi)
+
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O3 -qarch=auto")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -qarch=auto")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -qsimd=auto -qhot=novector -qnostrict -g -qreport -qsource -qlist")
 
 set(PKG_PATH "/usr/gapps/bdiv/${SYS_TYPE}/opt")
 
