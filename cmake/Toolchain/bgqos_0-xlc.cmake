fb6e590ed0f76d1880dc176aead554a39fe3f0ad
Adam J. Kunen <kunen1@llnl.gov>
2014-07-02 11:32:16 -0700

    Added HTML compiler reports for xlc

--- cmake/Toolchain/bgqos_0-xlc.cmake
+++ cmake/Toolchain/bgqos_0-xlc.cmake
@@ -13,7 +13,7 @@ set(CMAKE_LINKER /usr/local/tools/compilers/ibm/mpixlcxx_r-lompbeta2-fastmpi)
 
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O3 -qarch=auto")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -qsimd=auto -qhot=novector -qnostrict -g -qreport -qsource -qlist")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -qsimd=auto -qhot=novector -qnostrict -g -qreport -qsource -qlist -qlistfmt=html")
 
 set(PKG_PATH "/usr/gapps/bdiv/${SYS_TYPE}/opt")
 
