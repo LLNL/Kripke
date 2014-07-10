ab11cf4a1fa503a4c2760d7ea7983b0b0eda1f97
Adam J. Kunen <kunen1@llnl.gov>
2014-07-10 09:16:18 -0700

    Fixed PAPI implementation to use less buggy API calls, added ICC vectorization report

--- cmake/Library/papi.cmake
+++ cmake/Library/papi.cmake
@@ -1,7 +1,7 @@
 
-set(papi_DEFAULT_INCLUDE_DIRS "${papi_PREFIX}/static/include")
-
-set(papi_DEFAULT_LIB_DIR "${papi_PREFIX}/static/lib")
+set(papi_PREFIX "/usr/local/tools/papi")
+set(papi_DEFAULT_INCLUDE_DIRS "${papi_PREFIX}/include")
+set(papi_DEFAULT_LIB_DIR "${papi_PREFIX}/lib")
 set(papi_DEFAULT_LIBS "papi")
 
 set(papi_DEFAULT_DEFINITIONS "-DKRIPKE_USE_PAPI")
