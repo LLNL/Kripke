b1f64e8c6fb73d015fd7bb575a0e0cdb3637e1ef
Adam J. Kunen <kunen1@llnl.gov>
2015-04-01 11:47:21 -0700

    Added a default linux/mpicc toolchain environment for outside of LLNL

---
+++ cmake/Toolchain/linux-gcc.cmake
@@ -0,0 +1,10 @@
+set(CMAKE_C_COMPILER "mpicc")
+set(CMAKE_CXX_COMPILER "mpic++")
+set(CMAKE_LINKER "mpic++")
+
+set(CMAKE_C_FLAGS "-g -mtune=native ${CMAKE_C_FLAGS}")
+set(CMAKE_CXX_FLAGS "-g -std=c++11 -mtune=native ${CMAKE_CXX_FLAGS}")
+
+set(PKG_PATH "/usr/local")
+
+
