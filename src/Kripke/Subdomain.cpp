63467e5b182afbbd09f09b0672c9a0a2317c217a
Adam J. Kunen <kunen1@llnl.gov>
2015-04-11 18:15:29 -0700

    Backed out my changes to Leopold's hyperplane setup, due to changes made in master

--- src/Kripke/Subdomain.cpp
+++ src/Kripke/Subdomain.cpp
@@ -325,26 +325,26 @@ void Subdomain::computeSweepIndexSet(void){
 
       if ( i_inc == 1){
         i_min = sweep_block.start_i;
-        i_max = sweep_block.end_i;
+        i_max = sweep_block.end_i-1;
       }
       else{
-        i_min = sweep_block.end_i;
+        i_min = sweep_block.end_i+1;
         i_max = sweep_block.start_i;
       }
       if ( j_inc == 1){
         j_min = sweep_block.start_j;
-        j_max = sweep_block.end_j;
+        j_max = sweep_block.end_j-1;
       }
       else{
-        j_min = sweep_block.end_j;
+        j_min = sweep_block.end_j+1;
         j_max = sweep_block.start_j;
       }
       if ( k_inc == 1){
         k_min = sweep_block.start_k;
-        k_max = sweep_block.end_k;
+        k_max = sweep_block.end_k-1;
       }
       else{
-        k_min = sweep_block.end_k;
+        k_min = sweep_block.end_k+1;
         k_max = sweep_block.start_k;
       }
       int ii_tmp = (1 - i_inc)/2*i_max;
