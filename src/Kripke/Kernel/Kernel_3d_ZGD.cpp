902710c3781545217d08144f6cf1571b35736c78
Adam J. Kunen <kunen1@llnl.gov>
2015-09-13 00:38:17 -0700

    Moved over ZGD to Views and Layouts

--- src/Kripke/Kernel/Kernel_3d_ZGD.cpp
+++ src/Kripke/Kernel/Kernel_3d_ZGD.cpp
@@ -33,6 +33,8 @@
 #include<Kripke/Kernel/Kernel_3d_ZGD.h>
 #include<Kripke/Grid.h>
 #include<Kripke/SubTVec.h>
+#include<Domain/Layout.h>
+
 
 Nesting_Order Kernel_3d_ZGD::nestingPsi(void) const {
   return NEST_ZGD;
@@ -73,18 +75,16 @@ void Kernel_3d_ZGD::LTimes(Grid_Data *grid_data) {
     Subdomain &sdom = grid_data->subdomains[sdom_id];
 
     // Get dimensioning
-    int num_groups = sdom.phi->groups;
     int num_zones = sdom.num_zones;
+    int num_groups = sdom.phi->groups;
     int num_local_groups = sdom.num_groups;
     int group0 = sdom.group0;
     int num_local_directions = sdom.num_directions;
-    int num_gnm = num_groups * num_moments;
-    int num_locgd = num_local_groups * num_local_directions;
 
     // Get pointers
-    double const * KRESTRICT ell = sdom.ell->ptr();
-    double const * KRESTRICT psi = sdom.psi->ptr();
-    double       * KRESTRICT phi = sdom.phi->ptr();
+    View3d<double, LAYOUT_KJI> psi(sdom.psi->ptr(), num_local_directions, num_local_groups, num_zones);
+    View3d<double, LAYOUT_KJI> phi(sdom.phi->ptr(), num_moments, num_groups, num_zones);
+    View2d<double, LAYOUT_JI>  ell(sdom.ell->ptr(), num_local_directions, num_moments);
 
 #ifdef KRIPKE_USE_OPENMP
 #pragma omp parallel for
@@ -93,9 +93,7 @@ void Kernel_3d_ZGD::LTimes(Grid_Data *grid_data) {
       for (int g = 0; g < num_local_groups; ++g) {
         for(int nm = 0;nm < num_moments;++nm){
           for (int d = 0; d < num_local_directions; d++){
-            phi[z*num_gnm + (group0+g)*num_moments + nm] += 
-              ell[nm*num_local_directions + d] * 
-              psi[z*num_locgd + g*num_local_directions + d];
+            phi(nm, g+group0, z) += ell(d,nm) * psi(d,g,z);
           }
         }
       }
@@ -120,17 +118,15 @@ void Kernel_3d_ZGD::LPlusTimes(Grid_Data *grid_data) {
 
     // Get dimensioning
     int num_zones = sdom.num_zones;
-    int num_groups = sdom.phi->groups;
     int num_local_groups = sdom.num_groups;
+    int num_groups = sdom.phi_out->groups;
     int group0 = sdom.group0;
     int num_local_directions = sdom.num_directions;
-    int num_gnm = num_moments*num_groups;
-    int num_locgd = num_local_directions*num_local_groups;
-
+    
     // Get pointers
-    double const * KRESTRICT phi_out = sdom.phi_out->ptr();
-    double const * KRESTRICT ell_plus = sdom.ell_plus->ptr();
-    double       * KRESTRICT rhs = sdom.rhs->ptr();
+    View3d<double, LAYOUT_KJI> rhs(sdom.rhs->ptr(), num_local_directions, num_local_groups, num_zones);
+    View3d<double, LAYOUT_KJI> phi_out(sdom.phi_out->ptr(), num_moments, num_groups, num_zones);
+    View2d<double, LAYOUT_IJ>  ell_plus(sdom.ell_plus->ptr(), num_local_directions, num_moments);
 
 #ifdef KRIPKE_USE_OPENMP
 #pragma omp parallel for
@@ -139,9 +135,7 @@ void Kernel_3d_ZGD::LPlusTimes(Grid_Data *grid_data) {
       for (int g = 0; g < num_local_groups; ++g) {
         for (int d = 0; d < num_local_directions; d++) {
           for(int nm = 0;nm < num_moments; ++nm){
-            rhs[z*num_locgd + g*num_local_directions + d] += 
-              ell_plus[d*num_moments + nm] * 
-              phi_out[z*num_gnm + (group0+g)*num_moments + nm];
+            rhs(d,g,z) += ell_plus(d,nm) * phi_out(nm,g+group0,z);
           }          
         }        
       }     
@@ -166,44 +160,42 @@ void Kernel_3d_ZGD::scattering(Grid_Data *grid_data){
     // get material mix information
     int sdom_id = grid_data->zs_to_sdomid[zs];
     Subdomain &sdom = grid_data->subdomains[sdom_id];
-    int    const * KRESTRICT zones_to_mixed = &sdom.zones_to_mixed[0];
-    int    const * KRESTRICT num_mixed = &sdom.num_mixed[0];
-    int    const * KRESTRICT mixed_material = &sdom.mixed_material[0];
-    double const * KRESTRICT mixed_fraction = &sdom.mixed_fraction[0];
-    double const * KRESTRICT sigs = grid_data->sigs->ptr();
-
-    int    const * KRESTRICT moment_to_coeff = &grid_data->moment_to_coeff[0];
-    double const * KRESTRICT phi = grid_data->phi[zs]->ptr();
-    double       * KRESTRICT phi_out = grid_data->phi_out[zs]->ptr();
 
     // grab dimensions
     int num_zones = sdom.num_zones;
     int num_groups = grid_data->phi_out[zs]->groups;
     int num_moments = grid_data->total_num_moments;
-    int num_coeff = grid_data->legendre_order+1;
-    int num_nmg = num_moments*num_groups;
+    int legendre_order = grid_data->legendre_order;
+
+    View3d<double, LAYOUT_KJI> phi_out(sdom.phi_out->ptr(), num_moments, num_groups, num_zones);
+    View3d<double, LAYOUT_KJI> const phi(sdom.phi->ptr(), num_moments, num_groups, num_zones);  
+    View4d<double, LAYOUT_LJKI> const sigs(grid_data->sigs->ptr(), legendre_order+1, num_groups, num_groups, 3);
+
+    View1d<int, LAYOUT_I> const zones_to_mixed(&sdom.zones_to_mixed[0], 1);
+    View1d<int, LAYOUT_I> const num_mixed(&sdom.num_mixed[0], 1);
+    View1d<int, LAYOUT_I> const mixed_material(&sdom.mixed_material[0], 1);
+    View1d<double, LAYOUT_I> const mixed_fraction(&sdom.mixed_fraction[0], 1);
+    View1d<int, LAYOUT_I> const moment_to_coeff(&grid_data->moment_to_coeff[0], 1);
 
 #ifdef KRIPKE_USE_OPENMP
 #pragma omp parallel for
 #endif
     for(int zone = 0;zone < num_zones;++ zone){
-      int mix_start = zones_to_mixed[zone];
-      int mix_stop = mix_start + num_mixed[zone];
+      int mix_start = zones_to_mixed(zone);
+      int mix_stop = mix_start + num_mixed(zone);
 
       for(int mix = mix_start;mix < mix_stop;++ mix){
-        int material = mixed_material[mix];
-        double fraction = mixed_fraction[mix];
+        int material = mixed_material(mix);
+        double fraction = mixed_fraction(mix);
 
         for(int g = 0;g < num_groups;++ g){      
           for(int gp = 0;gp < num_groups;++ gp){
             for(int nm = 0;nm < num_moments;++ nm){
               // map nm to n
-              int n = moment_to_coeff[nm];
+              int n = moment_to_coeff(nm);
               
-              phi_out[zone*num_nmg + gp*num_moments + nm] += 
-                sigs[material*num_coeff*num_groups*num_groups + g*num_groups*num_coeff + gp*num_coeff + n] * 
-                phi[zone*num_nmg + g*num_moments + nm] * 
-                fraction;
+              phi_out(nm, gp, zone) += 
+                sigs(n, g, gp, material) * phi(nm, g, zone) * fraction;                     
             }
           }        
         }
@@ -225,44 +217,35 @@ void Kernel_3d_ZGD::source(Grid_Data *grid_data){
     // get material mix information
     int sdom_id = grid_data->zs_to_sdomid[zs];
     Subdomain &sdom = grid_data->subdomains[sdom_id];
-    int    const * KRESTRICT zones_to_mixed = &sdom.zones_to_mixed[0];
-    int    const * KRESTRICT num_mixed = &sdom.num_mixed[0];
-    int    const * KRESTRICT mixed_material = &sdom.mixed_material[0];
-    double const * KRESTRICT mixed_fraction = &sdom.mixed_fraction[0];
-    double       * KRESTRICT phi_out = grid_data->phi_out[zs]->ptr();
 
     // grab dimensions
+    int num_mixed = sdom.mixed_to_zones.size();
     int num_zones = sdom.num_zones;
     int num_groups = grid_data->phi_out[zs]->groups;
     int num_moments = grid_data->total_num_moments;
+    
+    View3d<double, LAYOUT_KJI> phi_out(sdom.phi_out->ptr(), num_moments, num_groups, num_zones);
+    View1d<int,    LAYOUT_I> const mixed_to_zones(&sdom.mixed_to_zones[0], 1);
+    View1d<int,    LAYOUT_I> const mixed_material(&sdom.mixed_material[0], 1);
+    View1d<double, LAYOUT_I> const mixed_fraction(&sdom.mixed_fraction[0], 1);
 
 #ifdef KRIPKE_USE_OPENMP
 #pragma omp parallel for
 #endif
-    for(int zone = 0;zone < num_zones;++ zone){
-      int mix_start = zones_to_mixed[zone];
-      int mix_stop = mix_start + num_mixed[zone];
-
-      for(int mix = mix_start;mix < mix_stop;++ mix){
-        int material = mixed_material[mix];
-        double fraction = mixed_fraction[mix];
+    for(int mix = 0;mix < num_mixed;++ mix){
+      int zone = mixed_to_zones(mix);
+      int material = mixed_material(mix);
+      double fraction = mixed_fraction(mix);
         
-        if(material == 0){        
-          for(int g = 0;g < num_groups;++ g){
-            phi_out[zone*num_moments*num_groups + g*num_moments] += 1.0 * fraction;
-          }
+      if(material == 0){        
+        for(int g = 0;g < num_groups;++ g){
+          phi_out(0, g, zone) += 1.0 * fraction;        
         }
       }
     }
   }
 }
 
-// Macros for offsets with fluxes on cell faces 
-#define I_PLANE_INDEX(j, k) ((k)*(local_jmax) + (j))
-#define J_PLANE_INDEX(i, k) ((k)*(local_imax) + (i))
-#define K_PLANE_INDEX(i, j) ((j)*(local_imax) + (i))
-#define Zonal_INDEX(i, j, k) ((i) + (local_imax)*(j) \
-  + (local_imax)*(local_jmax)*(k))
 
 void Kernel_3d_ZGD::sweep(Subdomain *sdom) {
   int num_directions = sdom->num_directions;
@@ -275,19 +258,27 @@ void Kernel_3d_ZGD::sweep(Subdomain *sdom) {
   int local_jmax = sdom->nzones[1];
   int local_kmax = sdom->nzones[2];
 
-  double const * KRESTRICT dx = &sdom->deltas[0][0];
-  double const * KRESTRICT dy = &sdom->deltas[1][0];
-  double const * KRESTRICT dz = &sdom->deltas[2][0];
+  View1d<double, LAYOUT_I> const dx(&sdom->deltas[0][0], local_imax+2);
+  View1d<double, LAYOUT_I> const dy(&sdom->deltas[1][0], local_jmax+2);
+  View1d<double, LAYOUT_I> const dz(&sdom->deltas[2][0], local_kmax+2);
   
-  double const * KRESTRICT sigt = sdom->sigt->ptr();
-  double       * KRESTRICT psi  = sdom->psi->ptr();
-  double const * KRESTRICT rhs  = sdom->rhs->ptr();
+  View3d<double, LAYOUT_KJI> const rhs(sdom->rhs->ptr(), num_directions, num_groups, num_zones);
+  View3d<double, LAYOUT_KJI> psi(sdom->psi->ptr(), num_directions, num_groups, num_zones);
+  View2d<double, LAYOUT_JI>  const sigt(sdom->sigt->ptr(), num_groups, num_zones);
 
-  double * KRESTRICT psi_lf = sdom->plane_data[0]->ptr();
-  double * KRESTRICT psi_fr = sdom->plane_data[1]->ptr();
-  double * KRESTRICT psi_bo = sdom->plane_data[2]->ptr();
-
-  int num_gd = num_groups * num_directions;
+  int num_z_i = local_jmax * local_kmax;
+  int num_z_j = local_imax * local_kmax;
+  int num_z_k = local_imax * local_jmax;  
+  
+  View3d<double, LAYOUT_KJI> psi_lf(sdom->plane_data[0]->ptr(), num_directions, num_groups, num_z_i);
+  View3d<double, LAYOUT_KJI> psi_fr(sdom->plane_data[1]->ptr(), num_directions, num_groups, num_z_j);
+  View3d<double, LAYOUT_KJI> psi_bo(sdom->plane_data[2]->ptr(), num_directions, num_groups, num_z_k);
+  
+  Layout3d<LAYOUT_KJI> zone_layout(local_imax, local_jmax, local_kmax);
+  Layout2d<LAYOUT_JI> i_layout(local_jmax, local_kmax);
+  Layout2d<LAYOUT_JI> j_layout(local_imax, local_kmax);
+  Layout2d<LAYOUT_JI> k_layout(local_imax, local_jmax);
+  
 
   // All directions have same id,jd,kd, since these are all one Direction Set
   // So pull that information out now
@@ -302,29 +293,29 @@ void Kernel_3d_ZGD::sweep(Subdomain *sdom) {
 #endif
         for (int g = 0; g < num_groups; ++g) {
           for (int d = 0; d < num_directions; ++d) {
-            double const xcos_dxi = 2.0 * direction[d].xcos / dx[i + 1];
-            double const ycos_dyj = 2.0 * direction[d].ycos / dy[j + 1];
-            double const zcos_dzk = 2.0 * direction[d].zcos / dz[k + 1];
+            double const xcos_dxi = 2.0 * direction[d].xcos / dx(i + 1);
+            double const ycos_dyj = 2.0 * direction[d].ycos / dy(j + 1);
+            double const zcos_dzk = 2.0 * direction[d].zcos / dz(k + 1);
             
-            int const z = Zonal_INDEX(i, j, k);
-            int const lf_idx = I_PLANE_INDEX(j, k)*num_gd + g*num_directions + d;
-            int const fr_idx = J_PLANE_INDEX(i, k)*num_gd + g*num_directions + d;
-            int const bo_idx = K_PLANE_INDEX(i, j)*num_gd + g*num_directions + d;
+            int const z_idx = zone_layout(i,j,k);            
+            int const lf_idx = i_layout(j,k);
+            int const fr_idx = j_layout(i,k);
+            int const bo_idx = k_layout(i,j);
+
+            /* Calculate new zonal flux */
+            double const psi_d_g_z = (
+                  rhs(d,g,z_idx)                  
+                + psi_lf(d,g,lf_idx) * xcos_dxi
+                + psi_fr(d,g,fr_idx) * ycos_dyj
+                + psi_bo(d,g,bo_idx) * zcos_dzk)
+                / (xcos_dxi + ycos_dyj + zcos_dzk + sigt(g,z_idx) );
+
+            psi(d,g,z_idx) = psi_d_g_z;
             
-            // Calculate new zonal flux 
-            double const psi_z_g_d = (
-                rhs[z*num_gd + g*num_directions + d]
-                + psi_lf[lf_idx] * xcos_dxi
-                + psi_fr[fr_idx] * ycos_dyj
-                + psi_bo[bo_idx] * zcos_dzk)
-                / (xcos_dxi + ycos_dyj + zcos_dzk + sigt[z*num_groups + g]);
-
-            psi[z*num_gd + g*num_directions + d] = psi_z_g_d;
-
-            // Apply diamond-difference relationships 
-            psi_lf[lf_idx] = 2.0 * psi_z_g_d - psi_lf[lf_idx];
-            psi_fr[fr_idx] = 2.0 * psi_z_g_d - psi_fr[fr_idx];
-            psi_bo[bo_idx] = 2.0 * psi_z_g_d - psi_bo[bo_idx];
+            /* Apply diamond-difference relationships */
+            psi_lf(d,g,lf_idx) = 2.0 * psi_d_g_z - psi_lf(d,g,lf_idx);
+            psi_fr(d,g,fr_idx) = 2.0 * psi_d_g_z - psi_fr(d,g,fr_idx);
+            psi_bo(d,g,bo_idx) = 2.0 * psi_d_g_z - psi_bo(d,g,bo_idx);
           }
         }
       }
