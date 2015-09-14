5ef371effd24283574355bf21ef17a46d2d541a3
Adam J. Kunen <kunen1@llnl.gov>
2015-09-14 12:58:20 -0700

    Started playing with a Nesting_Order policy

--- src/Kripke/Kernel/Kernel_3d_DGZ.cpp
+++ src/Kripke/Kernel/Kernel_3d_DGZ.cpp
@@ -60,17 +60,27 @@ Nesting_Order Kernel_3d_DGZ::nestingSigs(void) const {
   return NEST_DGZ;
 }
 
-
-struct ltimes_dgz_pol {
-  typedef LAYOUT_IJKL layout;
-  typedef seq_pol pol_i;
-  typedef seq_pol pol_j;
-  typedef omp_pol pol_k;
-  typedef seq_pol pol_l;
+struct dgz_pol{
+  typedef LAYOUT_IJK layout_psi;
+  typedef LAYOUT_IJK layout_phi;
+  typedef LAYOUT_JI layout_ell;
+  
+  typedef View3d<double, layout_psi> View3d_Psi;
+  typedef View3d<double, layout_psi> View3d_Phi;
+  typedef View2d<double, layout_ell> View2d_Ell;
+  
+  struct ltimes_pol {
+    typedef LAYOUT_IJKL layout;
+    typedef seq_pol pol_i;
+    typedef seq_pol pol_j;
+    typedef omp_pol pol_k;
+    typedef seq_pol pol_l;
+  };
 };
 
+template<typename POL>
+void genLTimes(Grid_Data *grid_data){
 
-void Kernel_3d_DGZ::LTimes(Grid_Data *grid_data) {
   // Outer parameters
   int num_moments = grid_data->total_num_moments;
 
@@ -92,15 +102,23 @@ void Kernel_3d_DGZ::LTimes(Grid_Data *grid_data) {
     int num_local_directions = sdom.num_directions;
     
     // Get pointers    
-    View3d<double, LAYOUT_IJK> psi(sdom.psi->ptr(), num_local_directions, num_local_groups, num_zones);
-    View3d<double, LAYOUT_IJK> phi(sdom.phi->ptr(), num_moments, num_groups, num_zones);
-    View2d<double, LAYOUT_JI>  ell(sdom.ell->ptr(), num_local_directions, num_moments);
+    typename POL::View3d_Psi psi(sdom.psi->ptr(), num_local_directions, num_local_groups, num_zones);
+    typename POL::View3d_Phi phi(sdom.phi->ptr(), num_moments, num_groups, num_zones);
+    typename POL::View2d_Ell ell(sdom.ell->ptr(), num_local_directions, num_moments);
     
-    forall4<ltimes_dgz_pol>(num_moments, num_local_directions, num_local_groups, num_zones,
+    forall4<typename POL::ltimes_pol>(num_moments, num_local_directions, num_local_groups, num_zones,
       [&](int nm, int d, int g, int z){
         phi(nm, g+group0, z) += ell(d,nm) * psi(d,g,z);
       });
-  } 
+  }
+}
+
+
+void Kernel_3d_DGZ::LTimes(Grid_Data *grid_data) {
+//  Nesting_Order nest = NEST_DGZ;
+  // TODO:  How do we map runtime nest order to template parameters, and not have to 
+  // write map functions for every kernel?
+  genLTimes<dgz_pol>(grid_data);
 }
 
 void Kernel_3d_DGZ::LPlusTimes(Grid_Data *grid_data) {
