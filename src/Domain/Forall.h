a0880bebf6e86369db1f0f71180dbd37405c37ed
Adam J. Kunen <kunen1@llnl.gov>
2015-11-10 09:36:19 -0800

    Added ability to specialize a policyScope based on specific tag

--- src/Domain/Forall.h
+++ src/Domain/Forall.h
@@ -82,22 +82,90 @@ RAJA_INLINE void forallZoneSets(Grid_Data *grid_data, BODY const &body){
 
 
 
+
+// Foreward decl
+template<typename TAG, typename BODY>
+RAJA_INLINE void executeScope(BODY const &body);
+
+// Foreward decl
+template<typename TAG, typename T1, typename L1, typename... REST>
+RAJA_INLINE void executeScope(T1 t1, L1 const &l1, REST... rest);
+
+
+template<typename T, typename TAG>
+struct ExecuteIfMatches{
+    template<typename BODY>
+    RAJA_INLINE void match(BODY const &body) const {
+      // nop
+    }
+
+    template<typename... REST>
+    RAJA_INLINE void not_match(REST... rest) const {
+      executeScope<TAG, REST...>(rest...);
+    }
+};
+
+
+template<typename T>
+struct ExecuteIfMatches<T, T>{
+    template<typename BODY>
+    RAJA_INLINE void match(BODY const &body) const {
+      body(T());
+    }
+
+    template<typename... REST>
+    RAJA_INLINE void not_match(REST... rest) const {
+      // NOP
+    }
+};
+
+
 /**
- * Converts run-time nesting to static type for a given lambda scope.
- * This requires C++14 for polymorphic lambdas
+ * Termination case: labmda is always executed
+ *
+ * This handles the default policyScope lambd
  */
-template<typename BODY>
-inline void policyScope(Nesting_Order nesting_order, BODY const &body){
-  switch(nesting_order){
-    case NEST_DGZ: body(NEST_DGZ_T()); break;
-    case NEST_DZG: body(NEST_DZG_T()); break;
-    case NEST_GDZ: body(NEST_GDZ_T()); break;
-    case NEST_GZD: body(NEST_GZD_T()); break;
-    case NEST_ZDG: body(NEST_ZDG_T()); break;
-    case NEST_ZGD: body(NEST_ZGD_T()); break;
-  }
+template<typename TAG, typename BODY>
+RAJA_INLINE void executeScope(BODY const &body){
+  body(TAG());
 }
 
+/**
+ * Executes the L1 lambda with parameter type T1 if the types T1 and TAG are the same.
+ *
+ * If they are not the same, it recursively calls itself with the remaining types and arguments in REST... and rest...
+ */
+template<typename TAG, typename T1, typename L1, typename... REST>
+RAJA_INLINE void executeScope(T1 t1, L1 const &l1, REST... rest){
+  ExecuteIfMatches<T1, TAG> test;
+  test.match(l1); // execute l1(T1()) if it matches
+  test.not_match(rest...); // otherwise, try the remaining specializations
+}
+
+
+/**
+ * Converts run-time nesting to static type for a given lambda scope.
+ * This requires C++14 for polymorphic lambdas.
+ *
+ * First argument is the nesting order variable.
+ * Followed by pairs of specializations [nest_tag(), labmda(nest_tag)],
+ * Terminated by a single default lambda(auto nest_tag).
+ *
+ * The specializations are executed if any of their types match the nesting order tag.
+ * The default is executed if none of the specializations match.
+ *
+ */
+template<typename... REST>
+RAJA_INLINE void policyScope(Nesting_Order nest, REST... rest){
+  switch(nest){
+    case NEST_DGZ: executeScope<NEST_DGZ_T>(rest...); break;
+    case NEST_DZG: executeScope<NEST_DZG_T>(rest...); break;
+    case NEST_GDZ: executeScope<NEST_GDZ_T>(rest...); break;
+    case NEST_GZD: executeScope<NEST_GZD_T>(rest...); break;
+    case NEST_ZDG: executeScope<NEST_ZDG_T>(rest...); break;
+    case NEST_ZGD: executeScope<NEST_ZGD_T>(rest...); break;
+  }
+}
 
 
 #endif
