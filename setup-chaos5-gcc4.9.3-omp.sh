d65c30b7c911a803d6dcaec588a8b315f96dc543
Adam J. Kunen <kunen1@llnl.gov>
2015-11-12 19:51:44 -0800

    Fixed setup scripts to use GNU not GCC

--- setup-chaos5-gcc4.9.3-omp.sh
+++ setup-chaos5-gcc4.9.3-omp.sh
@@ -3,5 +3,5 @@
 . /usr/local/tools/dotkit/init.sh
 use gcc-4.9.3p
 
-CXX=mpig++ CXXFLAGS="-O3 -g -fopenmp -mtune=native -std=c++14 -DRAJA_PLATFORM_X86_SSE -DRAJA_COMPILER_GCC -DKRIPKE_USE_OPENMP" cmake $1 -DENABLE_MPI=Off
+CXX=mpig++ CXXFLAGS="-O3 -g -fopenmp -mtune=native -std=c++14 -DRAJA_PLATFORM_X86_SSE -DRAJA_COMPILER_GNU -DKRIPKE_USE_OPENMP" cmake $1 -DENABLE_MPI=Off
 
