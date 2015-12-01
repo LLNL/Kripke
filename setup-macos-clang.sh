122bb569b2830cd54bba3bc5c6f5159c7b3ad5b0
Adam J. Kunen <kunen1@llnl.gov>
2015-11-30 17:10:37 -0800

    Merged in some stuff form coe-1.1, normalized the build process somewhat. Cleaned up the cmake/toolchain file stuff

--- setup-macos-clang.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-CXX=mpic++ CXXFLAGS="-Wall -O3 -g -std=c++14 -DRAJA_PLATFORM_X86_SSE -DRAJA_COMPILER_CLANG" cmake $1 -DENABLE_MPI=Off
-
--- src/RAJA/sources/MemUtils.cxx
+++ src/RAJA/sources/MemUtils.cxx
@@ -27,6 +27,7 @@
 
 #include<string>
 #include<iostream>
+#include<stdlib.h>
 
 namespace RAJA {
 
