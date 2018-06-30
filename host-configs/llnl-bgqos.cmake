a933607170b992df00364d1d5ace3a91d44242dd
Adam J. Kunen <kunen1@llnl.gov>
2018-06-30 13:36:06 -0700

    Fixed build for BG/Q at LLNL

--- host-configs/llnl-bgqos.cmake
+++ host-configs/llnl-bgqos.cmake
@@ -11,6 +11,7 @@ set(RAJA_COMPILER "RAJA_COMPILER_CLANG" CACHE STRING "")
 
 set(CMAKE_C_COMPILER   "/usr/apps/gnu/clang/2017.06.06/llnl/bin/mpiclang" CACHE PATH "")
 set(CMAKE_CXX_COMPILER "/usr/apps/gnu/clang/2017.06.06/llnl/bin/mpiclang++" CACHE PATH "")
+set(CMAKE_LINKER       "/usr/apps/gnu/clang/2017.06.06/llnl/bin/mpiclang++" CACHE PATH "")
 
 set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3 -ffast-math -std=c++11 -stdlib=libc++" CACHE STRING "")
 set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} -O3 -ffast-math -std=c++11 -stdlib=libc++" CACHE STRING "")
@@ -20,7 +21,7 @@ set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0 -std=c++11 -stdlib=libc+
 set(KRIPKE_ARCH "OpenMP" CACHE STRING "")
 
 set(ENABLE_OPENMP On CACHE BOOL "")
-set(ENABLE_MPI On CACHE BOOL "")
+set(ENABLE_MPI_WRAPPER On CACHE BOOL "")
 
 set(RAJA_RANGE_ALIGN 4 CACHE INT "")
 set(RAJA_RANGE_MIN_LENGTH 32 CACHE INT "")
