3338ffd5cd04ddd9e10fbb1324f61e99bcabd52c
Adam J. Kunen <kunen1@llnl.gov>
2018-10-12 12:18:34 -0700

    More fixes for P100 and V100 platforms

--- host-configs/llnl-blueos-V100-nvcc-clang.cmake
+++ host-configs/llnl-blueos-V100-nvcc-clang.cmake
@@ -12,14 +12,14 @@ set(RAJA_COMPILER "RAJA_COMPILER_CLANG" CACHE STRING "")
 set(CMAKE_C_COMPILER   "mpiclang" CACHE PATH "")
 set(CMAKE_CXX_COMPILER "mpiclang++" CACHE PATH "")
 
-set(CMAKE_CXX_FLAGS "-std=c++14 -stdlib=libc++" CACHE STRING "")
+set(CMAKE_CXX_FLAGS "" CACHE STRING "")
 set(CMAKE_CXX_FLAGS_RELEASE "-O3 -ffast-math" CACHE STRING "")
 set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O3 -g -ffast-math" CACHE STRING "")
 set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g" CACHE STRING "")
 
 set(ENABLE_CHAI On CACHE BOOL "")
 set(ENABLE_CUDA On CACHE BOOL "")
-set(ENABLE_OPENMP On CACHE BOOL "")
+set(ENABLE_OPENMP Off CACHE BOOL "")
 set(ENABLE_MPI_WRAPPER On CACHE BOOL "")
 
 set(CUDA_NVCC_FLAGS -restrict;-gencode=arch=compute_70,code=sm_70;-std c++14;--expt-extended-lambda; -ccbin; ${CMAKE_CXX_COMPILER} CACHE LIST "")
