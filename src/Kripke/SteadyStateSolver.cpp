837e7cc6e56b8dc0a704886c49dab5463a3c9c56
brandonneth <nethbrandone@gmail.com>
2019-11-11 10:59:05 -0700

    fixed two typos within comments. Variable names in the comments did not match program variables nor variables in documentation. They now do.

--- src/Kripke/SteadyStateSolver.cpp
+++ src/Kripke/SteadyStateSolver.cpp
@@ -55,18 +55,18 @@ int Kripke::SteadyStateSolver (Kripke::Core::DataStore &data_store, size_t max_i
 
 
 
-    // Compute Scattering Source Term (psi_out = S*phi)
+    // Compute Scattering Source Term (phi_out = S*phi)
     Kripke::Kernel::kConst(data_store.getVariable<Kripke::Field_Moments>("phi_out"), 0.0);
     Kripke::Kernel::scattering(data_store);
 
 
 
-    // Compute External Source Term (psi_out = psi_out + Q)
+    // Compute External Source Term (phi_out = phi_out + Q)
     Kripke::Kernel::source(data_store);
 
 
 
-    // Moments to Discrete transformation (rhs = LPlus*psi_out)
+    // Moments to Discrete transformation (rhs = LPlus*phi_out)
     Kripke::Kernel::kConst(data_store.getVariable<Kripke::Field_Flux>("rhs"), 0.0);
     Kripke::Kernel::LPlusTimes(data_store);
 
