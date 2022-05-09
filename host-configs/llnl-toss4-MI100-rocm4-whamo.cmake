3c3ae67a8ab68d5875a0ca45c41ac53e8052d8d6
Robert Chen <chen59@llnl.gov>
2022-05-09 11:55:45 -0700

    RZWHAMO arch update.

--- host-configs/llnl-toss4-MI100-rocm4-whamo.cmake
+++ host-configs/llnl-toss4-MI100-rocm4-whamo.cmake
@@ -11,13 +11,15 @@
 
 set(RAJA_COMPILER "RAJA_COMPILER_CLANG" CACHE STRING "")
 
-set(CMAKE_C_COMPILER   "amdclang" CACHE PATH "")
-set(CMAKE_CXX_COMPILER "amdclang++" CACHE PATH "")
+set(CMAKE_C_COMPILER   "/opt/rocm-4.5.2/llvm/bin/clang" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "/opt/rocm-4.5.2/llvm/bin/clang++" CACHE PATH "")
 
 set(CMAKE_CXX_FLAGS "" CACHE STRING "")
 set(CMAKE_CXX_FLAGS_RELEASE "-std=c++14 -O3 -ffast-math" CACHE STRING "")
 set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-std=c++14 -O3 -g -ffast-math" CACHE STRING "")
 set(CMAKE_CXX_FLAGS_DEBUG "-std=c++14 -O0 -g" CACHE STRING "")
+
+set(CMAKE_HIP_ARCHITECTURES "--offload-arch=gfx906,gfx908" CACHE STRING "")
 
 set(ENABLE_CHAI On CACHE BOOL "")
 set(ENABLE_HIP On CACHE BOOL "")
