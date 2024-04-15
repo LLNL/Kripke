d0d54e5deb0ef374d0e47a84c2c6d1db8ec4f3eb
Robert Chen <chen59@llnl.gov>
2024-04-15 14:37:10 -0700

    Update ROCM build configs.

--- host-configs/llnl-toss4-MI300A-rocm6-adams.cmake
+++ host-configs/llnl-toss4-MI300A-rocm6-adams.cmake
@@ -5,20 +5,24 @@
 # SPDX-License-Identifier: (BSD-3-Clause)
 #
 
-# module load rocmcc/5.6.0-cce-16.0.0a-magic
-# module load rocm/5.6.0
+# module load rocmcc/6.0.3-cce-17.0.1-magic
 
 set(RAJA_COMPILER "RAJA_COMPILER_CLANG" CACHE STRING "")
 
-set(CMAKE_C_COMPILER   "/usr/tce/packages/cray-mpich/cray-mpich-8.1.26-rocmcc-5.6.0-cce-16.0.0a-magic/bin/mpicc" CACHE PATH "")
-set(CMAKE_CXX_COMPILER "/usr/tce/packages/cray-mpich/cray-mpich-8.1.26-rocmcc-5.6.0-cce-16.0.0a-magic/bin/mpicxx" CACHE PATH "")
+set(CMAKE_C_COMPILER   "/usr/tce/packages/cray-mpich/cray-mpich-8.1.29-rocmcc-6.0.3-cce-17.0.1-magic/bin/mpiamdclang" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "/usr/tce/packages/cray-mpich/cray-mpich-8.1.29-rocmcc-6.0.3-cce-17.0.1-magic/bin/mpiamdclang++" CACHE PATH "")
 
 set(CMAKE_CXX_FLAGS "" CACHE STRING "")
 set(CMAKE_CXX_FLAGS_RELEASE "-std=c++14 -O3 -ffast-math" CACHE STRING "")
 set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-std=c++14 -O3 -g -ffast-math" CACHE STRING "")
 set(CMAKE_CXX_FLAGS_DEBUG "-std=c++14 -O0 -g" CACHE STRING "")
 
-set(CMAKE_HIP_ARCHITECTURES "--offload-arch=gfx90a" CACHE STRING "")
+set(ROCM_ROOT_DIR "/opt/rocm-6.0.3" CACHE PATH "")
+set(HIP_ROOT_DIR "/opt/rocm-6.0.3/hip" CACHE PATH "")
+set(HIP_PATH "/opt/rocm-6.0.3/llvm/bin" CACHE PATH "")
+set(CMAKE_HIP_ARCHITECTURES "gfx942" CACHE STRING "")
+set(GPU_TARGETS "gfx942" CACHE STRING "")
+set(AMD_GPU_TARGETS "gfx942" CACHE STRING "")
 
 set(ENABLE_CHAI On CACHE BOOL "")
 set(ENABLE_HIP On CACHE BOOL "")
