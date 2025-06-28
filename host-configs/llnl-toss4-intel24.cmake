87b72da72e8a11993c6671a8887e8626ddcdd0a8
Robert Chen <chen59@llnl.gov>
2025-06-27 17:50:47 -0700

    Add build config for Intel 2024.

---
+++ host-configs/llnl-toss4-intel24.cmake
@@ -0,0 +1,23 @@
+#
+# Copyright (c) 2014-23, Lawrence Livermore National Security, LLC
+# and Kripke project contributors. See the Kripke/COPYRIGHT file for details.
+# 
+# SPDX-License-Identifier: (BSD-3-Clause)
+#
+
+# module load intel/2024.2.1-magic
+
+set(RAJA_COMPILER "RAJA_COMPILER_INTEL" CACHE STRING "")
+
+set(CMAKE_C_COMPILER   "/usr/tce/packages/mvapich2/mvapich2-2.3.7-intel-2024.2.1-magic/bin/mpicc" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "/usr/tce/packages/mvapich2/mvapich2-2.3.7-intel-2024.2.1-magic/bin/mpicxx" CACHE PATH "")
+
+set(CMAKE_CXX_FLAGS "" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_RELEASE "-O3" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O3 -g" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g" CACHE STRING "")
+
+set(ENABLE_OPENMP On CACHE BOOL "")
+set(ENABLE_MPI On CACHE BOOL "")
+
+
