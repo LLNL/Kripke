86368357ba2ad45e089417ed1797eca940addc82
Michael McKinsey <michaelmckinsey1@gmail.com>
2025-11-04 14:57:31 -0800

    Fix template

--- src/Kripke/Arch/LTimes.h
+++ src/Kripke/Arch/LTimes.h
@@ -243,7 +243,7 @@ template<>
 struct Policy_LTimes<ArchLayoutT<ArchT_CUDA, LayoutT_GDZ>> {
     using ExecPolicy =
       KernelPolicy<
-        CudaKernelFixedAsync<256,
+        RAJA::statement::CudaKernelFixedAsync<256,
           For<2, RAJA::cuda_global_size_z_direct<8>, // group
             For<0, RAJA::cuda_global_size_y_direct<8>, // moment
               For<1, RAJA::cuda_global_size_x_direct<32>, // direction
