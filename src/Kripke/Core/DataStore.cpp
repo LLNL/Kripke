4d1696a58f67dd7addab48725f0175b48311c9c5
Riyaz Haque <your-email@example.com>
2026-02-05 00:52:46 -0800

    Changes

--- src/Kripke/Core/DataStore.cpp
+++ src/Kripke/Core/DataStore.cpp
@@ -29,8 +29,9 @@ DataStore::DataStore(int dev_pool_size){
   auto chai_resource_manager = chai::ArrayManager::getInstance();
   chai_resource_manager->setAllocator(chai::GPU, dev_pool_allocator);
 #ifdef KRIPKE_USE_CHAI_SINGLE_MEMORY
-  // Use same Umpire allocator for chai::CPU and chai::GPU when using single memory
+  // Use same Umpire allocator for all execution spaces when using single memory
   chai_resource_manager->setAllocator(chai::CPU, dev_pool_allocator);
+  chai_resource_manager->setAllocator(chai::NONE, dev_pool_allocator);
 #endif
   // force allocation of GPU memory pool
   auto tmp = new chai::ManagedArray<int>(100, chai::GPU);
