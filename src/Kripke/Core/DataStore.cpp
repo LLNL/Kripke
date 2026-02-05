d86276a176a538900dc8f21ebc2a8f9e0589ebf5
Riyaz Haque <your-email@example.com>
2026-02-04 22:33:21 -0800

    Add an option for building CHAI in single memory mode

--- src/Kripke/Core/DataStore.cpp
+++ src/Kripke/Core/DataStore.cpp
@@ -28,7 +28,10 @@ DataStore::DataStore(int dev_pool_size){
   auto dev_pool_allocator = rm.makeAllocator<umpire::strategy::QuickPool>(allocator_name, rm.getAllocator("DEVICE"), umpire_dev_pool_size, umpire_dev_block_size);
   auto chai_resource_manager = chai::ArrayManager::getInstance();
   chai_resource_manager->setAllocator(chai::GPU, dev_pool_allocator);
+#ifdef KRIPKE_USE_CHAI_SINGLE_MEMORY
+  // Use same Umpire allocator for chai::CPU and chai::GPU when using single memory
   chai_resource_manager->setAllocator(chai::CPU, dev_pool_allocator);
+#endif
   // force allocation of GPU memory pool
   auto tmp = new chai::ManagedArray<int>(100, chai::GPU);
   tmp->free(chai::GPU);
