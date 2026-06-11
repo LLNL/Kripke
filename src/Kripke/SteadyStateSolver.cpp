d0ee21693451a176e734b447b0e18772d9dd24ce
Robert Chen <chen59@llnl.gov>
2026-06-10 19:31:57 -0700

    Change accuracy back to 6 decimal places.

--- src/Kripke/SteadyStateSolver.cpp
+++ src/Kripke/SteadyStateSolver.cpp
@@ -106,7 +106,7 @@ int Kripke::SteadyStateSolver (Kripke::Core::DataStore &data_store, size_t max_i
      */
     double part = Kripke::Kernel::population(data_store);
     if(comm.rank() == 0){
-      printf("  iter %d: particle count=%.10e, change=%.10e\n", (int)iter, part, (part-part_last)/part);
+      printf("  iter %d: particle count=%.6e, change=%.6e\n", (int)iter, part, (part-part_last)/part);
       fflush(stdout);
     }
     part_last = part;
